f41466d43b25dce8dda69881bb61e2d02bc30fa0
Stable <55439243+xenfovn@users.noreply.github.com>
2021-09-21 16:17:29 +0700

    basic job billing

--- config.lua
+++ config.lua
@@ -1,5 +1,7 @@
 Config = Config or {}
 Config.BillingCommissions = { -- This is a percentage (0.10) == 10%
+    mechanic = 0.10,
+    police = 0.10,
     mechanic = 0.10
 }
 Config.RepeatTimeout = 2000
